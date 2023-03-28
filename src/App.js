aadc3052ee2c8e7a2b2e643b9b161691ddd4ebe0
jorgeduje <jorgeduje4@gmail.com>
2023-03-28 18:52:50 -0300

    se limpia el codigo que no se usa

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
+import "./App.css";
 
-import './App.css';
-
-import { Login } from "./Login"
 
 function App() {
   return (
     <div className="App">
-     <Login />
-     <h1>Esto es app.</h1>
-     <h3>Probando los cambios y el pull</h3>
+      <h1>Home</h1>
     </div>
   );
 }
 
 export default App;
-
