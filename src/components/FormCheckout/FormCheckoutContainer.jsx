35b162e8bc0a5285ffc95e120229d491b6b4db7c
jorgeduje <jorgeduje4@gmail.com>
2023-05-02 21:01:16 -0300

    se trabaja con firebase

---
+++ src/components/FormCheckout/FormCheckoutContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FormCheckout from './FormCheckout'
+
+export const FormCheckoutContainer = () => {
+  return (
+    <div>
+        <FormCheckout />
+    </div>
+  )
+}
