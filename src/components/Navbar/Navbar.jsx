c31b74852631ad8a27a9d0b969bff0d16f7fc3e1
jorgeduje <jorgeduje4@gmail.com>
2023-05-03 22:16:00 -0300

    se trabaja con formik y yup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,6 @@ import { useEffect, useState } from "react";
 
 export const Navbar = () => {
   const [categories, setCategories] = useState([]);
-  console.log(categories);
 
   useEffect(() => {
     const categoriesCollection = collection(db, "categories");
